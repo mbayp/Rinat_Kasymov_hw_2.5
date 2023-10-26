39437b26eb1255626d00e65c21199908f1db2358
mbayp <mbayp13@gmail.com>
2023-10-26 18:47:37 +0600

    Initial commit

---
+++ app/src/main/java/com/example/rinat_kasymov_hw_25/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.rinat_kasymov_hw_25;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
